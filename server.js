bf33d8e334976a5359591a7efece60436c576e69
gabkings <gabworks51@gmail.com>
2025-05-11 16:30:57 +0300

    Adding cors configs

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ connectDB()
 
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}else{
+    app.use(cors({ origin: `${process.env.CLIENT_URL_PROD}` }));
 }
 // app.use(cors())
 app.use("/api",blogRoutes)
